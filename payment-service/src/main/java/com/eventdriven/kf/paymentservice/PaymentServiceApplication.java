9347d5c5fe350449f8898bc8b0cf9d3462229f8b
srinivas-tatraju <tsr.5n.dev@gmail.com>
2026-04-24 21:08:51 +0530

    Kafka retry with backoff and DLT handler implemented

--- payment-service/src/main/java/com/eventdriven/kf/paymentservice/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/eventdriven/kf/paymentservice/PaymentServiceApplication.java
@@ -2,7 +2,9 @@ package com.eventdriven.kf.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @SpringBootApplication
 public class PaymentServiceApplication {
 
